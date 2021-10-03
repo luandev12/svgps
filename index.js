ce3dd8eb103a30aa720d8e9ba6dd94e9f5a7a4c8
Tran Luan <91442498+luandev12@users.noreply.github.com>
2021-10-03 09:02:10 +0700

    v2

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const serverless = require('serverless-http');
+const axios = require('axios');
 const FileUpload = require('express-fileupload');
-const morgan = require('morgan');
-const cluster = require('cluster');
 
-const os = require('os').cpus().length;
-const numCluster = os;
-const PORT = 8081;
+const port = 8080;
+
 const app = express();
-
-const router = require('./routers');
-
 const fileUpload = {
-  limits: { fileSize: 50 * 1024 * 500024 },
+  limits: { fileSize: 50 * 1024 * 1024 },
 };
 
-if (cluster.isMaster) {
-  for (let i = 0; i < numCluster; i++) {
-    cluster.fork();
-    console.log(`The Worker number: ${i + 1} is alive`);
+app.use(cors());
+app.use(FileUpload(fileUpload));
+
+app.post('/svg/:type', (req, res) => {
+  const { type } = req.params;
+  const files = req.files.file;
+  const url = `https://vector.express/api/v2/public/convert/svg/librsvg`;
+
+  if (type === 'ps' || type === 'eps') {
+    axios({
+      url: `${url}/${type}`,
+      method: 'POST',
+      data: files.data,
+    })
+      .then(({ data }) => {
+        res.json({
+          id: data.id,
+          inputUrl: data.inputUrl,
+          resultUrl: data.resultUrl,
+          time: data.time,
+          format: data.format,
+        });
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
   }
-  cluster.on('exit', (worker) => {
-    console.log(`The Worker number: ${worker.id} has died`);
-    cluster.fork();
-  });
-} else {
-  app.use(cors({ credentials: true, origin: true, exposedHeaders: '*' }));
-  app.use(FileUpload(fileUpload));
-  app.use(morgan('dev'));
+});
 
-  app.use(express.urlencoded({ extended: false, limit: '50mb' }));
-  app.use(express.json({ type: '*/*', limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.json({ type: '*/*', limit: '50mb' }));
 
-  app.use('/api', router);
-  app.get('/a', (req, res) => {
-    console.log(`Worker ${process.pid} started`);
-    res.send('Hello World!, This is Render Server!');
-  });
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-  app.listen(PORT, () => {
-    console.log(`Worker ${process.pid} started`);
-    console.log(`Timezones by location application is running on port ${PORT}.`);
-  });
-}
+module.exports.server = serverless(app);
